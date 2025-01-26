0a48375e31da1d88f2d2dc38a96175e79d1680e1
Christopher Nunez <elgary1129@gmail.com>
2025-01-26 00:11:07 -0300

    Entidad para las Url y su repositorios creados

---
+++ src/main/java/com/url_shortening/repository/UrlRepository.java
@@ -0,0 +1,12 @@
+package com.url_shortening.repository;
+
+import com.url_shortening.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortUrl(String shortUrl);
+}
---
+++ src/main/java/com/url_shortening/service/UrlService.java
@@ -0,0 +1,7 @@
+package com.url_shortening.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlService {
+}
